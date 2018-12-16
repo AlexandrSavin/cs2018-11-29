8918009cfb9f71fcdeb3f0b4dada4e055dd4b1ad
alexandrsavin <Alexandrsavin2015@gmail.com>
2018-12-16 22:23:00 +0300

    lesson 6

--- src/by/it/savin/lesson05/TaskA1.java
+++ src/by/it/savin/lesson05/TaskA1.java
@@ -29,8 +29,14 @@ import java.util.Scanner;
 public class TaskA1 {
 
     public static void main (String[] arge) {
-        int[] array=new int[10];
-        Scanner sc=new Scanner(System.in);
+        int[] mas=new int[10];
+        Scanner scanner=new Scanner(System.in);
+        for (int i = 9; i >= 0; i--) {
+            mas [i]=scanner.nextInt();
+        }
+        for (int element : mas) {
+            System.out.println(element);
+        }
 
 
     }
